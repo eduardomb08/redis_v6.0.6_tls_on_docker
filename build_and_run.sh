5083c2f0febd0800de1362feb6ba42c64363b8d6
Ubuntu <azureuser@redis-tls.gvjk5f1wyhoejfrwf0ovfjreea.cx.internal.cloudapp.net>
2020-07-29 20:40:45 +0000

    fixed redis-cli file, removed unecessary file and fixed docker run parameters

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,3 +1,3 @@
 sudo docker build . -t redis-tls
 
-sudo docker run --name redis-tls -v 6379.conf:/usr/local/etc/redis/6379.conf -p 6379:6379 -d redis-tls redis-server /usr/local/etc/redis/6379.conf
+sudo docker run --name redis-tls -v ~/redis-tls/6379.conf:/usr/local/etc/redis/6379.conf -v ~/redis-tls/tests/tls/:/ssl/certs -p 6379:6379 -d redis-tls redis-server /usr/local/etc/redis/6379.conf
